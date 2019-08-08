47dd7757a7fd260b048abea3c904d5ecc0c10909
xiexiupeng <xxp564357196@163.com>
2019-08-08 15:25:41 +0800

    实现一个双向队列

--- quenue.js
+++ quenue.js
@@ -48,3 +48,30 @@ function eempty(){
         return  false
     }
 }
+
+/* 学完队列之后，我们自己来实现一个双向队列，允许从队列的两端添加和删除元素，并写一段测试程序测试该类 */
+/**
+ * @classdesc 在上面其实我们已经实现了在队尾添加元素和在队首删除元素
+ * @classdesc 那么其实我们差的几乎在队首删除元素和在队尾添加元素，其他功能都是一样的
+ */
+function Deque(){
+    this.dataSource=[];
+    this.enqueue=enqueue; // 队尾添加元素
+    this.enFirst=enFirst; // 在队首添加元素
+    this.dequeue=dequeue; // 在队首删除元素
+    this.DeEnd=DeEnd; // 在队尾删除元素
+    this.toString=toString;
+    this.eempty=eempty;
+}
+// 下面我们着重来实现enFirst和 DeEnd
+
+// 队首添加元素,在es6中有unshit方法向数组的队首添加元素
+function enFirst(element){
+    this.dataSource.unshift(element)
+}
+// 队尾删除元素
+function DeEnd(){
+    return this.dataSource.pop()
+}
+
+// 其他方法与queue一致，就不重复了
